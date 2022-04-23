d787815e7b4d6a213003f59ecdc0a5a6dcaae98c
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2022-04-23 16:37:13 +0530

    pipe

--- src/app/shared/pipes/sort.pipe.ts
+++ src/app/shared/pipes/sort.pipe.ts
@@ -4,9 +4,38 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'sort'
 })
 export class SortPipe implements PipeTransform {
+  // items can be any array of objects, cities, orders, sales, items etc...
+  // propertyName is an attribute in object, name, cityName, etc
+  // sortType : asc , desc
+  transform(items: any[], propertyName: string, sortType: string = 'asc'): any[] {
+    console.log("Pipe is called ", items, propertyName, sortType)
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    if (!items || !propertyName) {
+      return items;
+    }
+
+    if (sortType == 'desc') {
+          return items.sort ( (leftItem, rightItem) => {
+            if (leftItem[propertyName] < rightItem[propertyName])
+              return 1;
+
+            return -1
+      })
+    }
+
+    // asc order
+   
+    return items.sort ( (leftItem, rightItem) => {
+      if (leftItem[propertyName] < rightItem[propertyName])
+        return -1;
+
+      return 1
+    })
+
+  
+    
+
+
   }
 
 }
